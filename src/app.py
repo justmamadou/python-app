b0d422f33820f42ed9e6275f83ec9b7678baa549
MamadouRichatt <mamadou.ba@richatt.com>
2025-09-03 15:47:06 +0000

    Create flask  app and dockerize it using docker

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+import socket
+import datetime 
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'message': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'hostname': socket.gethostname()
+        })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
