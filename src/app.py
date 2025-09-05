932115ac3ca88a643d1cf88ef15f8a86ac78a2c4
MamadouRichatt <mamadou.ba@richatt.com>
2025-09-05 21:15:34 +0000

    test sha short id

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'message': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Hello, World!'
+        'message': 'Hello, World2!'
         })
 
 @app.route('/api/v1/healthz')
