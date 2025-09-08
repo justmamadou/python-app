050b4025f693af0608c359fbc875f1ae8c86d903
MamadouRichatt <mamadou.ba@richatt.com>
2025-09-08 15:57:49 +0000

    try to fix argo env

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'message': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Hello, World4!'
+        'message': 'Hello, World5!'
         })
 
 @app.route('/api/v1/healthz')
