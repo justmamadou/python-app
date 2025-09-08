64a27a21118cd467ccb75c927200c64df9b8682b
MamadouRichatt <mamadou.ba@richatt.com>
2025-09-08 16:18:43 +0000

    fix errors

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'message': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Hello from ArgoCD!'
+        'message': 'Hello from ArgoCD2!'
         })
 
 @app.route('/api/v1/healthz')
