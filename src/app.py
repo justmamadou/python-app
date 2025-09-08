ad3e7463754a36c3ad982a45825906431772b049
MamadouRichatt <mamadou.ba@richatt.com>
2025-09-08 15:49:50 +0000

    test login to argocd in the workflow

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'message': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Hello, World3!'
+        'message': 'Hello, World4!'
         })
 
 @app.route('/api/v1/healthz')
